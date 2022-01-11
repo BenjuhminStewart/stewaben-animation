e462aff6b69dd8aa7199c25d24a06ecd17864c2e
Benjamin Stewart <82689821+BenjuhminStewart@users.noreply.github.com>
2022-01-11 14:50:12 -0800

    init changes

--- main.js
+++ main.js
@@ -2,11 +2,16 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
+ASSET_MANAGER.queueDownload("./sprites/blob idle.png");
+ASSET_MANAGER.queueDownload("./sprites/blob move.png");
+ASSET_MANAGER.queueDownload("./sprites/blob attack.png");
+ASSET_MANAGER.queueDownload("./sprites/blob death.png");
+
 ASSET_MANAGER.downloadAll(() => {
-	const canvas = document.getElementById("gameWorld");
-	const ctx = canvas.getContext("2d");
+  const canvas = document.getElementById("gameWorld");
+  const ctx = canvas.getContext("2d");
 
-	gameEngine.init(ctx);
+  gameEngine.init(ctx);
 
-	gameEngine.start();
+  gameEngine.start();
 });
